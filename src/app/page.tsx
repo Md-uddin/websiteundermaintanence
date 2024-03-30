3ee28919670da9e2c43afdea5be33675ff08e903
Md-uddin <easeanger@gmail.com>
2024-03-31 01:08:36 +0530

    added under construction page

---
+++ src/app/page.tsx
@@ -0,0 +1,48 @@
+import * as React from "react";
+
+interface CompanyInfoProps {
+  name: string;
+  email: string;
+  phone: string;
+}
+
+function CompanyInfo({ name, email, phone }: CompanyInfoProps) {
+  return (
+    <div className="flex gap-5 self-stretch max-md:flex-wrap max-md:max-w-full text-black">
+      <div className="flex-auto self-start">{name}</div>
+      <div className="flex flex-col items-start">
+        <div className="flex items-center justify-center">
+          <div className="mr-2 font-bold">Email:</div>
+          <a href="mailto:contact@crea8iveclick.com"> {email}</a>
+        </div>
+        <div className="flex items-center justify-center">
+          <div className="mr-2 font-bold">Phone:</div>
+          <a href="tel:+919581506305"> {phone}</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default function Home() {
+  return (
+    <div
+      className="flex flex-col items-end px-14 pt-12 pb-20 w-full max-md:px-5 max-md:max-w-full bg-center bg-cover bg-no-repeat h-screen"
+      style={{
+        background: "url('bg.png')",
+      }}
+    >
+      <CompanyInfo
+        name="Crea8iveClick"
+        email="contact@crea8iveclick.com"
+        phone="+91 9581506305"
+      />
+      <h1 className="mt-80 mr-24 text-6xl text-right w-[958px] max-md:mt-10 max-md:mr-2.5 max-md:max-w-full max-md:text-4xl ">
+        Oops! This website is currently under construction!
+      </h1>
+      <p className="mt-9 mr-24 mb-80 font-light text-right max-md:mr-2.5 max-md:mb-10 ">
+        Stay Tuned!
+      </p>
+    </div>
+  );
+}
