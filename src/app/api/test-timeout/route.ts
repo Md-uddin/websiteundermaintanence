816e8112a95e04d79191d2588d0fc2543a537dcd
Md-uddin <easeanger@gmail.com>
2024-07-11 18:57:01 +0530

    fix:small issues

--- src/app/api/test-timeout/route.ts
+++ src/app/api/test-timeout/route.ts
@@ -1,9 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest, res: NextResponse) {
+export async function GET(request: NextRequest): Promise<NextResponse> {
 	return new Promise((resolve) => {
 		setTimeout(() => {
-			return resolve(
+			resolve(
 				NextResponse.json({
 					success: true,
 					message: "Hi buddy",
