fcc6b5ace104ed22b5df0acb09b6e0f6ad10c880
Md-uddin <easeanger@gmail.com>
2024-07-19 14:50:02 +0530

    smll updt

--- src/app/(auth)/auth/callback/route.ts
+++ src/app/(auth)/auth/callback/route.ts
@@ -12,7 +12,7 @@ export async function GET(request: Request) {
 		const supabase = createClient();
 		const { error } = await supabase.auth.exchangeCodeForSession(code);
 		if (!error) {
-			return NextResponse.redirect(`${origin}${next}`);
+			return NextResponse.redirect(`https://crea8iveclick.com${next}`);
 		}
 	}
 
